900460932835d4a081fa58a7f9a285064e0281ef
Steve Wan <swan@swan-mba.corp.dropbox.com>
2015-04-19 11:08:59 -0700

    Initial Commit

---
+++ tips2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips2
+//
+//  Created by Steve Wan on 4/19/15.
+//  Copyright (c) 2015 Steve Wan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
